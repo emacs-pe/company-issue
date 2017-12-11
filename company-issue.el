bed269007eefc68e3dfe3d0e2d33a69bd39f859f
Mario Rodas <marsam@users.noreply.github.com>
2017-12-10 19:30:34 -0500

    Remove clutter in company backend init

--- company-issue.el
+++ company-issue.el
@@ -82,8 +82,10 @@
 
 Where slug is stripped from its \".git\" suffix and \"/\"
 prefix."
-  (let ((urlobj (company-issue-git-parse-url url)))
-    (list (url-host urlobj) (string-remove-prefix "/" (string-remove-suffix ".git" (url-filename urlobj))))))
+  (if (and url (not (string-blank-p url)))
+      (let ((urlobj (company-issue-git-parse-url url)))
+        (list (url-host urlobj) (string-remove-prefix "/" (string-remove-suffix ".git" (url-filename urlobj)))))
+    (user-error "Invalid git remote url: %S" url)))
 
 (defun company-issue-load-object (filename)
   "Read obj from FILENAME."
@@ -91,27 +93,31 @@ prefix."
     (insert-file-contents filename)
     (read (current-buffer))))
 
-(cl-defun company-issue-issues (&optional (host company-issue-host) (slug company-issue-slug))
-  "Read company issues from cache from HOST and SLUG."
-  (and host slug
+(defun company-issue-issues ()
+  "Read company issues from cache."
+  (and company-issue-host company-issue-slug
        (or (gethash company-issue-root company-issue-project)
            (let* ((cache-file (expand-file-name "company-issue" (magit-git-dir)))
                   (from-cache (file-exists-p cache-file))
                   (issues (if from-cache
                               (company-issue-load-object cache-file)
-                            (company-issue-fetch (company-issue-as-symbol host) slug))))
+                            (company-issue-fetch (company-issue-as-symbol company-issue-host) company-issue-slug))))
              (or from-cache (with-temp-file cache-file (pp issues (current-buffer))))
              (puthash company-issue-root issues company-issue-project)))))
 
-;;;###autoload
-(defun company-issue-setup (&optional force)
-  "Read config from git, when FORCE is non-nil will thy for ask again."
-  (interactive "P")
-  (and (or force (not (magit-get "company-issue" "host")))
-       (cl-multiple-value-bind (host slug)
-           (company-issue-parse-remote-url (magit-get "remote" (magit-read-remote "Remote" nil 'use-only) "url"))
-         (magit-set host "company-issue" "host")
-         (magit-set slug "company-issue" "slug"))))
+(defun company-issue-setup ()
+  "Setup `company-issue' config."
+  (interactive)
+  (when-let (toplevel (magit-toplevel))
+    (or (and company-issue-host company-issue-slug)
+        (cl-multiple-value-setq (company-issue-host company-issue-slug)
+          (list (magit-get "company-issue" "host") (magit-get "company-issue" "slug"))))
+    (unless (and company-issue-host company-issue-slug)
+      (cl-multiple-value-setq (company-issue-host company-issue-slug)
+        (company-issue-parse-remote-url (magit-get "remote" (magit-read-remote "Remote" nil 'use-only) "url")))
+      (magit-set company-issue-host "company-issue" "host")
+      (magit-set company-issue-slug "company-issue" "slug"))
+    (setq company-issue-root toplevel)))
 
 (cl-defstruct (company-issue-item (:constructor company-issue-item-new))
   "A structure holding the information of an issue."
@@ -122,7 +128,10 @@ prefix."
 
 (defun company-issue-prefix ()
   "Get a prefix from current position."
-  (cons (company-grab "#\\([[:digit:]]*\\)" 1) t))
+  (if (bound-and-true-p company-issue-root)
+      (let ((prefix (company-grab "#\\([[:digit:]]*\\)" 1)))
+        (if prefix (cons prefix t) 'stop))
+    'stop))
 
 (defun company-issue-candidates (prefix)
   "Return issues candidates for current project with PREFIX."
@@ -148,11 +157,8 @@ Provide completion info according to COMMAND and ARG.  IGNORED, not used."
   (interactive (list 'interactive))
   (cl-case command
     (interactive (company-begin-backend 'company-issue))
-    (init (and (setq company-issue-root (magit-toplevel))
-               (or (and company-issue-host company-issue-slug)
-                   (cl-multiple-value-setq (company-issue-host company-issue-slug)
-                     (list (magit-get "company-issue" "host") (magit-get "company-issue" "slug"))))))
-    (prefix (or (company-issue-prefix) 'stop))
+    (init (company-issue-setup))
+    (prefix (company-issue-prefix))
     (candidates (company-issue-candidates arg))
     (meta (company-issue-meta arg))
     (annotation (company-issue-annotation arg))
